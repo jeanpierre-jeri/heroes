7b98fe0c9295cb4ecff274258b6fb81a7725a60d
Jean Pierre <jeanpierre.webdev@gmail.com>
2022-01-26 14:45:09 -0500

    Modificación en assets

--- src/components/hero/HeroCard.js
+++ src/components/hero/HeroCard.js
@@ -1,27 +1,38 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom'
 
+const heroImages = require.context('../../assets/heroes', true)
 
-const HeroCard = ({id, superhero, publisher, alter_ego, first_appearance, characters}) => {
+const HeroCard = ({
+  id,
+  superhero,
+  publisher,
+  alter_ego,
+  first_appearance,
+  characters,
+}) => {
   return (
     <div className="col animate__animated animate__fadeIn">
-      <div className="card h-100" style={{width: '18rem'}}>
-        <img src={`/assets/${id}.jpg`} className="card-img-top" alt={superhero} />
+      <div className="card h-100" style={{ width: '18rem' }}>
+        <img
+          src={heroImages(`./${id}.jpg`)}
+          className="card-img-top"
+          alt={superhero}
+        />
         <div className="card-body">
           <h5 className="card-title">{superhero}</h5>
           <p className="card-text">{alter_ego}</p>
           <p className="card-text">{first_appearance}</p>
-          <Link to={`/hero/${id}`} className="btn btn-primary">Ver más...</Link>
-        </div>  
-        {
-          (alter_ego !== characters) &&
+          <Link to={`/hero/${id}`} className="btn btn-primary">
+            Ver más...
+          </Link>
+        </div>
+        {alter_ego !== characters && (
           <div className="card-footer">
             <p className="text-muted">{characters}</p>
           </div>
-        }
+        )}
       </div>
     </div>
-    
-    
   )
 }
 
